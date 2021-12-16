91339c619596edb935d09ae77d587e5bb243ab78
StanTheAwesomeMan <80547533+StanTheAwesomeMan@users.noreply.github.com>
2021-12-16 19:59:39 +0100

    Main Files

---
+++ install.sh
@@ -0,0 +1,6 @@
+echo "installing..."
+
+sudo mv ./minifetch /usr/bin/minifetch
+sudo chmod +x /usr/bin/minifetch
+
+echo "installed successfully. run 'sudo rm /usr/bin/lolcat' to remove. you may now delete install.sh"
