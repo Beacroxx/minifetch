1b57d720ce4f5bdb8ae63dd7714b2298de139f39
StanTheAwesomeMan <80547533+StanTheAwesomeMan@users.noreply.github.com>
2022-02-14 21:16:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ echo "installing..."
 sudo mv ./minifetch /usr/bin/minifetch
 sudo chmod +x /usr/bin/minifetch
 
-echo "installed successfully. run 'sudo rm /usr/bin/lolcat' to remove. you may now delete install.sh"
+echo "installed successfully. run 'sudo rm /usr/bin/minifetch' to remove. you may now delete install.sh"
